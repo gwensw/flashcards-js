76917721be6dea1ec14628b21cb6f89fb80258e8
GwenSW <gwen.simswilliams@gmail.com>
2018-02-12 15:36:26 +0000

    Initial commit with library and tests

---
+++ tests/tinytest.js
@@ -0,0 +1,100 @@
+/**
+ * Very simple in-browser unit-test library, with zero deps.
+ *
+ * Background turns green if all tests pass, otherwise red.
+ * View the JavaScript console to see failure reasons.
+ *
+ * Example:
+ *
+ *   adder.js (code under test)
+ *
+ *     function add(a, b) {
+ *       return a + b;
+ *     }
+ *
+ *   adder-test.html (tests - just open a browser to see results)
+ *
+ *     <script src="tinytest.js"></script>
+ *     <script src="adder.js"></script>
+ *     <script>
+ *
+ *     tests({
+ *
+ *       'adds numbers': function() {
+ *         eq(6, add(2, 4));
+ *         eq(6.6, add(2.6, 4));
+ *       },
+ *
+ *       'subtracts numbers': function() {
+ *         eq(-2, add(2, -4));
+ *       },
+ *
+ *     });
+ *     </script>
+ *
+ * That's it. Stop using over complicated frameworks that get in your way.
+ *
+ * -Joe Walnes
+ * MIT License. See https://github.com/joewalnes/jstinytest/
+ */
+
+var TinyTest = {
+
+    run: function(tests) {
+        var failures = 0;
+      //for each key in the tests object...
+        for (var testName in tests) {
+          // save a reference to the method
+            var testAction = tests[testName];
+            try {
+              // call the callback, with TinyTest as 'this' (doesn't seem strictly necessary since the methods are all globals)
+                testAction.apply(this);
+              // log that it passed
+                console.log(`%cPASSED ${testName}`, 'color: green; background-color: palegreen');
+            } catch (e) {
+              // if the callback threw an error, increment failure count and log the error
+                failures++;
+                console.groupCollapsed(`%cFAILED ${testName}`, 'color: red; background-color: mistyrose; font-weight: normal');
+                console.error(e.stack);
+                console.groupEnd();
+            }
+        }
+        setTimeout(function() { // Give document a chance to complete, because callbacks passed into setTimeout only execute after main JS has cleared the stack
+          // check if document and body element have loaded...
+            if (window.document && document.body) {
+              	// change background to green if no failures counted, red if not
+                document.body.style.backgroundColor = (failures == 0 ? '#99ff99' : '#ff9999');
+            }
+        }, 0);
+    },
+
+    fail: function(msg) {
+        throw new Error('fail(): ' + msg);
+    },
+
+    assert: function(value, msg) {
+        if (!value) {
+            throw new Error('assert(): ' + msg);
+        }
+    },
+
+    assertEquals: function(expected, actual) {
+        if (expected != actual) {
+            throw new Error('assertEquals() "' + expected + '" != "' + actual + '"');
+        }
+    },
+
+    assertStrictEquals: function(expected, actual) {
+        if (expected !== actual) {
+            throw new Error('assertStrictEquals() "' + expected + '" !== "' + actual + '"');
+        }
+    },
+
+};
+
+var fail               = TinyTest.fail.bind(TinyTest),
+    assert             = TinyTest.assert.bind(TinyTest),
+    assertEquals       = TinyTest.assertEquals.bind(TinyTest),
+    eq                 = TinyTest.assertEquals.bind(TinyTest), // alias for assertEquals
+    assertStrictEquals = TinyTest.assertStrictEquals.bind(TinyTest),
+    tests              = TinyTest.run.bind(TinyTest);
